d025b6fb4e5e36a21540a73c02b4b90adb66c815
Arun <arun@mac.home>
2025-04-21 01:14:14 -0400

    chore: clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    esmExternals: 'loose',
-  },
-  webpack: (config) => {
-    config.module.noParse = /\.py$/;
-    return config;
-  },
-}
+};
 
 module.exports = nextConfig;
